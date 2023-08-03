91d3581db2dd2a983a1d23ecdd792fd5fc26253b
Goose better <114866826+bardium@users.noreply.github.com>
2023-08-03 11:22:25 -0400

    Update metadata.lua

--- metadata.lua
+++ metadata.lua
@@ -1,5 +1,5 @@
 return {
-	version = '1.07',
-	updated = '8/02/2023',
-	message = 'auto tp to safe zone'
+	version = '1.08',
+	updated = '8/03/2023',
+	message = 'black market webhook + bug fixes'
 }
